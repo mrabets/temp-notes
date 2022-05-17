5c2804161e8ce6ed2eed6e093bdd71547f9e15b9
Karim Marabet <newrabgo@gmail.com>
2022-05-17 12:37:43 +0300

    Add basic frontend page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
+	"text/template"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -12,7 +14,18 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Hello World"))
+	ts, err := template.ParseFiles("./ui/html/index.html")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
 
 func showNote(w http.ResponseWriter, r *http.Request) {
